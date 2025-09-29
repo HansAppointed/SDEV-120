a5dda9fdee3fee1e1661252ffc5e47b6dd575f40
HansAppointed <matthewkaye119@gmail.com>
2025-09-29 15:30:03 -0400

    Add files via upload

---
+++ M07 Assignment Functions in Python.py
@@ -0,0 +1,11 @@
+#This program determines if one number is greater than another number
+def greater_than(x,y):
+    if x > y:
+        return "true"
+    else:
+        return "false"
+a = 2
+b = 3
+
+result = greater_than(a,b)
+print(f"The statement {a} is more significant than {b} is {result}.")
